b060683249287f5aa7bd1e4df3aa5259da9974b2
freshbeka <rstiling@kingcounty.gov>
2026-04-09 09:49:14 -0700

    modified most field names to lower case

--- mapping_update_screening_tool_2026.R
+++ mapping_update_screening_tool_2026.R
@@ -26,10 +26,9 @@ PSSBmapping %>%
   group_by(alternate_name) %>% 
   filter(n() > 1) #2 duplicates
 
-#stopping here today #
+subset(mapping, is.na(otu_metric_calc))
 
-subset(mapping, is.na(OTU_MetricCalc))
-test<-subset(mapping, is.na(Preferred.Name)&Alternate.Name!=OTU_MetricCalc&OTU_MetricCalc!="DNI")
+test<-subset(mapping, is.na(preferred_name) & alternate_name!=otu_metric_calc & otu_metric_calc!="DNI")
 
 ##this function binds the textfiles from the taxa downloads from PSSB into one dataframe
 taxaBind <- function(file.path) {
@@ -45,86 +44,30 @@ taxaBind <- function(file.path) {
 
 file.path="./2026_screening_files/PSSB-visitdata/" ##data downloaded and current as of 04/06/2026-- all PSSB rivers and streams data
 raw<-taxaBind(file.path)
-
-#What happened to the other taxa that were not in BCG from before?
-raw %>% 
-  filter(Taxon == "Rhabdomastix (Rhabdomastix)")
-
-raw %>% 
-  filter(Taxon == "Cricotopus Sylvestris Group") #They are not observed
-
-raw %>% 
-  filter(Taxon == "Cricotopus sylvestris group") #They are not observed
-
-raw %>% 
-  filter(Taxon == "cricotopus sylvestris group") #They are not observed
-
-raw %>% 
-  filter(str_detect(Taxon, 'Cricotopus')) %>% 
-  select(Taxon, Taxon.Serial.Number) %>% 
-  unique()
-
-raw %>% 
-  filter(Taxon == "Procloeon venosum") #these are not either
-
-raw %>%  filter(Taxon == "Limnophilinae") %>%  
-  select(Taxon, Taxon.Serial.Number)
-
-raw %>%  filter(Taxon == "Rhyacophila Ecosa Group") %>%  
-  select(Taxon, Taxon.Serial.Number)
-
-raw %>%  filter(Taxon == "Desmona mono") %>%  
-  select(Taxon, Taxon.Serial.Number)
-
-raw %>%  filter(Taxon == "Hornectes quadrimaculatus") %>%  
-  select(Taxon, Taxon.Serial.Number)
-
-
-raw %>%  filter(Taxon == "Hydroporinae") %>%  
-  select(Taxon, Taxon.Serial.Number)
-
-raw %>% 
-  filter(str_detect(Taxon, 'omplex')) %>% 
-  select(Taxon, Taxon.Serial.Number) %>% 
-  unique()
-
-raw %>% 
-  filter(str_detect(Taxon, 'Rhabdomastix')) %>% 
-  select(Taxon, Taxon.Serial.Number) %>% 
-  unique()
-
-BCGmapping %>% 
-  filter(str_detect(Taxon_orig, 'Rhyacophila vetina'))
-
-BCGmapping %>% 
-  filter(str_detect(Taxon_orig, 'Rhabdomastix'))
-
 names(raw)
-PSSB_taxa<-unique(raw[,c(28, 29, 48:69)])
+PSSB_taxa<- raw |> select(Taxon.Serial.Number, Taxon, c(Rank:Subspecies)) |> unique()
+
 ##there are some repeat entries that somewhere in the hierarchy have an NA instead of "". This yields multiples of the same taxa. Fix this.
 PSSB_taxa[is.na(PSSB_taxa)]<-""
 PSSB_taxa<-unique(PSSB_taxa) #we're generating a list of all taxa in PSSB samples
 
-mapping<-merge(mapping, PSSB_taxa, by.x=c("Alternate.Name"), by.y=c("Taxon"), all.y=T) #merge the PSSB taxa list iwth the mapping dataframe, and limit the output to only taxa in the PSSB taxa list.
+mapping<-merge(mapping, PSSB_taxa, by.x=c("alternate_name"), by.y=c("Taxon"), all.y=T) #merge the PSSB taxa list iwth the mapping dataframe, and limit the output to only taxa in the PSSB taxa list.
 
-missingBCGmapping<-subset(mapping, is.na(OTU_MetricCalc), select="Alternate.Name")##these are taxa in PSSB samples that have not been translated by the BCG working group. 
-write.csv(missingBCGmapping, "Missing_from_BCG_taxa_translator.csv")
+missingBCGmapping<-subset(mapping, is.na(otu_metric_calc), select="alternate_name")##these are taxa in PSSB samples that have not been translated by the BCG working group. 
+write.csv(missingBCGmapping, "2026_screening_files/results/Missing_from_BCG_taxa_translator.csv")
 
-missingPSSBmapping<-subset(mapping, is.na(Preferred.Name)&((Alternate.Name!=OTU_MetricCalc&OTU_MetricCalc!="DNI")|is.na(OTU_MetricCalc)))##these are taxa in PSSB samples that don't have a mapping assigned in PSSB. 
+missingPSSBmapping<-subset(mapping, is.na(preferred_name)&((alternate_name!=otu_metric_calc&otu_metric_calc!="DNI")|is.na(otu_metric_calc)))##these are taxa in PSSB samples that don't have a mapping assigned in PSSB. 
 
-
-
-write.csv(missingPSSBmapping, "Missing_from_PSSB_mapping_table.csv")
-
+write.csv(missingPSSBmapping, "2026_screening_files/results/Missing_from_PSSB_mapping_table.csv")
 ###because there may be overlap between the last two, this next section will parse out the differences.
 
-missingPSSBmappingBCGexists<-missingPSSBmapping$Alternate.Name[!missingPSSBmapping$Alternate.Name %in% missingBCGmapping$Alternate.Name] ##these are taxa in PSSB samples that have a BCG translation, but do not have a mapping assigned in PSSB. Update the PSSB mapping to include these. 
+missingPSSBmappingBCGexists<-missingPSSBmapping$alternate_name[!missingPSSBmapping$alternate_name %in% missingBCGmapping$alternate_name] ##these are taxa in PSSB samples that have a BCG translation, but do not have a mapping assigned in PSSB. Update the PSSB mapping to include these. 
 
-missingBCG_PSSBmappingexists<-missingBCGmapping$Alternate.Name[missingBCGmapping$Alternate.Name %in% PSSBmapping$Alternate.Name]##these are taxa in PSSB samples that do not have a BCG translation, but do have a mapping assigned in PSSB. Probably a result of BAS editing some taxa names in PSSB for clarity, formatting and accuracy (i.e. Rhabdomastix (Rhabdomastix (Rhabdomastix)) was edited in PSSB taxonomy to simply Rhabdomastix (Rhabdomastix)). Let Sean know about these name changes in PSSB so he can update the BCG translation table with the edited names.
+missingBCG_PSSBmappingexists<-missingBCGmapping$alternate_name[missingBCGmapping$alternate_name %in% PSSBmapping$alternate_name]##these are taxa in PSSB samples that do not have a BCG translation, but do have a mapping assigned in PSSB. Probably a result of BAS editing some taxa names in PSSB for clarity, formatting and accuracy (i.e. Rhabdomastix (Rhabdomastix (Rhabdomastix)) was edited in PSSB taxonomy to simply Rhabdomastix (Rhabdomastix)). Let Sean know about these name changes in PSSB so he can update the BCG translation table with the edited names.
 
-missingBCGPSSB<-missingBCGmapping$Alternate.Name[missingBCGmapping$Alternate.Name %in% missingPSSBmapping$Alternate.Name]##these are taxa in PSSB samples that do not have a BCG translation, and do not have a mapping assigned in PSSB. Send these to Sean Sullivan once a year for updating the BCG translation tables.
+missingBCGPSSB<-missingBCGmapping$alternate_name[missingBCGmapping$alternate_name %in% missingPSSBmapping$alternate_name]##these are taxa in PSSB samples that do not have a BCG translation, and do not have a mapping assigned in PSSB. Send these to Sean Sullivan once a year for updating the BCG translation tables.
 
-missingBCGmapping$Alternate.Name[missingBCGmapping$Alternate.Name %in% missingPSSBmapping$Alternate.Name] ###this output should be identical to the object missingBCGPSSB
+missingBCGmapping$alternate_name[missingBCGmapping$alternate_name %in% missingPSSBmapping$alternate_name] ###this output should be identical to the object missingBCGPSSB
 
 ###this next section checks to see if existing mappings have changed since the last version of the BCG table.
 
