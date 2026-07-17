7c9b99cb8cd9446f78c4ffb70b0bfe242ce50fe0
freshbeka <rstiling@kingcounty.gov>
2026-07-16 17:32:05 -0700

    evaluating Truncatelloidea's history in PSSB

--- NZMS_related-taxonomy_2026.R
+++ NZMS_related-taxonomy_2026.R
@@ -36,3 +36,47 @@ all_taxa_unique <- all_taxa |> unique()
 
 truncatelloidea <- all_taxa_unique |> filter(Superfamily == "Truncatelloidea")
 NZMS <- all_taxa_unique |> filter(Genus == "Potamopyrgus")
+
+### Now I want to make a graph that has all the unknown locations on the y axis and time on the x axis. I want each square to be colored according to potamopyrgus present, Truncatelloidea present, or both. I'll need site, year, and two presence columns. That will allow me to make a third colulmn.
+
+stat <- read_csv("site-master_export/NZMS-status-2026.csv")
+unk_stat_names <- stat |> 
+  filter(NZMS != "yes") |> 
+  select("Site Name") |> 
+  pull()
+
+snail_observations_all <- raw |> 
+  select(Sample.ID, Site.Code, Visit.Date, Taxon) |> 
+  filter(Taxon == "Truncatelloidea" | Taxon == "Potamopyrgus antipodarum") |> 
+  mutate(presence = 1) |> # add a presence column
+  pivot_wider(names_from = Taxon, values_from = presence, values_fill = 0) |> 
+  rename(NZMS = "Potamopyrgus antipodarum") |> 
+  mutate(date =mdy(Visit.Date)) |> #change the character string to date
+  mutate(year = year(date)) |>  #extract year from the date
+  mutate(
+    species = case_when(
+    Truncatelloidea == 1 & NZMS == 0 ~ "Trun",
+    Truncatelloidea == 0 & NZMS == 1 ~ "NZMS",
+    Truncatelloidea == 1 & NZMS == 1 ~ "Both",
+  ))
+
+
+snail_observations <- snail_observations_all |> 
+  filter(Site.Code %in% unk_stat_names) 
+
+
+
+ggplot(snail_observations,aes(x = year, y = Site.Code, fill = species)) +
+  geom_tile() +
+  theme_minimal()
+
+ggplot(snail_observations_all,aes(x = year, y = Site.Code, fill = species)) +
+  geom_tile() +
+  theme_minimal()
+
+
+
+
+
+
+
